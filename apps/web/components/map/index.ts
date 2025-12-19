0eef250966cb2732c065d186bfe7cb040fbb92fb
VirajAnand-02 <iamtheviraj@gmail.com>
2025-12-19 20:27:42 +0530

    clean

--- apps/web/components/map/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Map Components
- * Export all map-related components
- */
-
-export { default as DynamicMap } from './DynamicMap';
-export { default as MapContainer } from './MapContainer';
-export type { MapContainerProps, MapMarker, MapPolyline } from './MapContainer';
--- apps/web/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Transpile shared packages
-  transpilePackages: ['@routesort/shared'],
-};
-
-export default nextConfig;
