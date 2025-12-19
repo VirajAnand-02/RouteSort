e7deb8395f5eca4bd0b3cf5516e92ba0e1b68e71
VirajAnand-02 <iamtheviraj@gmail.com>
2025-12-19 23:08:59 +0530

    inited

--- REFRENCE/driver-dashboard-mockup/components/chevron-down.tsx
+++
@@ -1,6 +0,0 @@
-import type React from "react"
-import { ChevronDownIcon } from "lucide-react"
-
-export function ChevronDown(props: React.SVGProps<SVGSVGElement>) {
-  return <ChevronDownIcon {...props} />
-}
